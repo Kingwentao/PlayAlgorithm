3e366e6d1a9657be976207ffa3b1a6da9b359a49
jinwentao <jinwentao@cvte.com>
2021-02-22 19:42:07 +0800

    start the algorithm learn

---
+++ src/main/java/com/wtk/playalgorithm/MyClass.java
@@ -0,0 +1,8 @@
+package com.wtk.playalgorithm;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        System.out.println("create java project to learn algorithm.");
+    }
+}
