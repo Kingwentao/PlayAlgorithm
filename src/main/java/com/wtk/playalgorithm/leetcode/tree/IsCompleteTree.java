62538d69da88070d95490efe5a6a27a00a26955e
wentaoKing <jinwentao@cvte.com>
2021-12-05 13:58:54 +0800

    添加完全二叉树检验

--- src/main/java/com/wtk/playalgorithm/leetcode/tree/IsCompleteTree.java
+++ src/main/java/com/wtk/playalgorithm/leetcode/tree/IsCompleteTree.java
@@ -1,5 +1,6 @@
 package com.wtk.playalgorithm.leetcode.tree;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -11,29 +12,54 @@ import java.util.Queue;
  * 核心思路：层级遍历（借助队列）+ 遍历过程遇到过空节点，并且当前节点不为空，此时不是完全二叉树
  */
 class IsCompleteTree {
+
     public static void main(String[] args) {
     }
 
-    public boolean isCompleteTree(TreeNode root) {
-        //note：层级遍历可借助队列存储下来
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        //定义：层次遍历过程是否遇到空节点
-        boolean isMeetNull = false;
-        while (!queue.isEmpty()) {
-            TreeNode cNode = queue.poll();
-            //遍历过程遇到过空节点，并且当前节点不为空，此时不是完全二叉树
-            if (isMeetNull && cNode != null) {
-                return false;
+    class Solution {
+        public boolean isCompleteTree(TreeNode root) {
+            //note：层级遍历可借助队列存储下来
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            //定义：层次遍历过程是否遇到空节点
+            boolean isMeetNull = false;
+            while (!queue.isEmpty()) {
+                TreeNode cNode = queue.poll();
+                //遍历过程遇到过空节点，并且当前节点不为空，此时不是完全二叉树
+                if (isMeetNull && cNode != null) {
+                    return false;
+                }
+                //层次遍历过程遇到空节点，标记下来
+                if (cNode == null) {
+                    isMeetNull = true;
+                    continue;
+                }
+                queue.offer(cNode.left);
+                queue.offer(cNode.right);
             }
-            //层次遍历过程遇到空节点，标记下来
-            if (cNode == null) {
-                isMeetNull = true;
-                continue;
-            }
-            queue.offer(cNode.left);
-            queue.offer(cNode.right);
+            return true;
         }
-        return true;
+
+        // 解法二(from 左程云b站课程)： 左空右不空 || 叶子结点层出现了左/右结点不为空的情况 -> false
+        public boolean isCompleteTree2(TreeNode root) {
+            if (root == null) return true;
+            Deque<TreeNode> deque = new LinkedList();
+            deque.offer(root);
+            boolean isLeaf = false;
+            while (!deque.isEmpty()) {
+                TreeNode node = deque.poll();
+                TreeNode lNode = node.left;
+                TreeNode rNode = node.right;
+                // 左空右不空 || 叶子结点层出现了左右结点不为空 -> false
+                if (lNode == null && rNode != null || isLeaf && (lNode != null || rNode != null))
+                    return false;
+                if (lNode != null) deque.offer(lNode);
+                if (rNode != null) deque.offer(rNode);
+                // 如果出现左右为空的情况，对于完全二叉树来说一定会是叶子结点层
+                if (rNode == null || lNode == null) isLeaf = true;
+            }
+            return true;
+        }
     }
+
 }
