142273b4fb1a043db18a010a95f3b007d02981f3
wentaoKing <jinwentao@cvte.com>
2021-12-07 22:13:56 +0800

    优化了二叉树的最近公共祖先的代码

--- src/main/java/com/wtk/playalgorithm/leetcode/tree/LowestCommonAncestor.java
+++ src/main/java/com/wtk/playalgorithm/leetcode/tree/LowestCommonAncestor.java
@@ -3,7 +3,7 @@ package com.wtk.playalgorithm.leetcode.tree;
 /**
  * author: created by wentaoKing
  * date: created in 5/2/21
- * description: 寻找二叉树的最近公共祖先
+ * description: 236.二叉树的最近公共祖先
  * <p>
  * 最近公共祖先的定义：
  * https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/236-er-cha-shu-de-zui-jin-gong-gong-zu-xian-hou-xu/
@@ -13,18 +13,17 @@ class LowestCommonAncestor {
     public static void main(String[] args) {
     }
 
-    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) return root;
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        //1.left、right都为空==》root的左右子树都不包含p、q
-        if (left == null && right == null) return null;
-        //2.left为空、right不为空 =》p、q都不在root的左子数上（分两种情况；都在右子树和其中一个在右子树）
-        if (left == null) return right;
-        //3.left不为空、right为空 =》p、q都不在root的右子数上（分两种情况；都在左子树和其中一个在左子树）
-        if (right == null) return left;
-        //4.left、right都不为空 ==》 p、q在root的左右两侧，root就是它们的最近公共祖先
-        return root;
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            // 如果head是p/q中的一个，那说明p/q就是公共祖先，即head是公共祖先
+            if (root == null || root == p || root == q) return root;
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            // p、q在root的左右两侧，root就是它们的最近公共祖先
+            if (left != null && right != null) return root;
+            // left不为空，right为空，说明left就是公共祖先，反之则为right
+            return left != null ? left : right;
+        }
     }
 
 }
